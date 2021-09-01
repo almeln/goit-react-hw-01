8daf940fed966e5e7bd57a30321396299dcb9cf4
Alina Melnyk <alinkaaa345@gmail.com>
2021-09-01 22:54:38 +0300

    homework done

---
+++ src/components/App.jsx
@@ -0,0 +1,25 @@
+import user from 'data/user.json';
+import Profile from 'components/SocialProfile/SocialProfile';
+import statisticalData from 'data/statistical-data.json';
+import Statistics from 'components/Statistics/Statistics';
+import friends from 'data/friends.json';
+import FriendList from 'components/FriendList/FriendList';
+import transactions from 'data/transactions.json';
+import TransactionHistory from './TransactionHistory/TransactionHistory';
+
+export default function App() {
+  return (
+    <div>
+      <Profile
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics stats={statisticalData} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />;
+    </div>
+  );
+}
